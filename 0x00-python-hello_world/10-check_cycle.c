61c17e29e85e89eb6726be7beddfe96631ef0bc7
TheTinyCoder <charawe.yw@gmail.com>
2023-10-03 09:59:55 +0300

    rewrite and test script that checks if linked list has a cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,21 +11,17 @@
 
 int check_cycle(listint_t *head)
 {
-	nodePtr temp1, temp2, temp3;
+	nodePtr temp1, temp2;
 
 	if (head)
 	{
-		if (head == head->next)
-			return (1);
-		for (temp1 = head->next; temp1; temp1 = temp1->next->next)
+		temp1 = temp2 = head;
+		while (temp1 && temp2 && temp2->next)
 		{
-			if (temp1->next)
-				temp3 = temp1->next;
-			for (temp2 = head; temp2 != temp1; temp2 = temp2->next)
-			{
-				if (temp2 == temp1->next || temp2 == temp3->next)
-					return (1);
-			}
+			temp1 = temp1->next;
+			temp2 = temp2->next->next;
+			if (temp1 == temp2)
+				return (1);
 		}
 	}
 	return (0);
