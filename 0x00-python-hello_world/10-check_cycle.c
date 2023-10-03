4e210f349d93489204ffd839d7f036da99e784c6
TheTinyCoder <charawe.yw@gmail.com>
2023-10-03 07:03:43 +0300

    fix runtime

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,17 +11,19 @@
 
 int check_cycle(listint_t *head)
 {
-	nodePtr temp1, temp2;
+	nodePtr temp1, temp2, temp3;
 
 	if (head)
 	{
 		if (head == head->next)
 			return (1);
-		for (temp1 = head->next; temp1; temp1 = temp1->next)
+		for (temp1 = head->next; temp1; temp1 = temp1->next->next)
 		{
+			if (temp1->next)
+				temp3 = temp1->next;
 			for (temp2 = head; temp2 != temp1; temp2 = temp2->next)
 			{
-				if (temp2 == temp1->next)
+				if (temp2 == temp1->next || temp2 == temp3->next)
 					return (1);
 			}
 		}
