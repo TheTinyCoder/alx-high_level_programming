a6467fae899d71f7f9766a2654940ee81ec94fa0
TheTinyCoder <charawe.yw@gmail.com>
2023-10-04 14:45:58 +0300

    write script that  creates a copy of the string, removing the character at the position n

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str1[len - 1]
+    j = 0
+    for i in range(len(str)):
+        if (i != n):
+            str1[j] = str[i]
+            j++
+    return (str1)
