73402aec9a8ccadf2f300058b9e88aa247a75240
TheTinyCoder <charawe.yw@gmail.com>
2023-10-05 19:21:55 +0300

    import a function from file and only execute module if main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(1, 2, add(a, b)))
-
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(1, 2, add(a, b)))
