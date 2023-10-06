1fe81e68b1f601549bcdca71654f1450c0f423a2
TheTinyCoder <charawe.yw@gmail.com>
2023-10-06 09:52:16 +0300

    refactor and test

---
+++ 0x02-python-import_modules/hidden_4_decompiled.py
@@ -0,0 +1,22 @@
+# uncompyle6 version 3.5.0
+# Python bytecode 3.8 (3413)
+# Decompiled from: Python 2.7.5 (default, Jun 20 2023, 11:36:40) 
+# [GCC 4.8.5 20150623 (Red Hat 4.8.5-44)]
+# Embedded file name: hidden_4.py
+# Size of source mod 2**32: 146 bytes
+
+
+def __init__():
+    return True
+
+
+def my_secret_santa():
+    return True
+
+
+def print_school():
+    return True
+
+
+def print_hidden():
+    return True
