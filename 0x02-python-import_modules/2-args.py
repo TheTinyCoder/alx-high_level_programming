9f76102366d94a255c717367a3523ad766b7f725
TheTinyCoder <charawe.yw@gmail.com>
2023-10-06 07:52:12 +0300

    refactor and test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     if (len(argv) == 1):
-        print("0 arguments")
+        print("0 arguments.")
     elif (len(argv) > 1):
         s = "s:" if len(argv) > 2 else ":"
         print("{} argument{}".format(len(argv) - 1, s))
