63c8ce17fe355a0859b43606a526e95cb8db2019
TheTinyCoder <charawe.yw@gmail.com>
2023-10-06 09:40:37 +0300

    refactor and test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4).sort()
     for name in names:
-        if (name[0] != '-'):
+        if (name[0] != '_'):
             print(name)
