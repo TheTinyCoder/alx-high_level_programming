8e3fe7b0f7287f5438f95e107083da12933a670d
TheTinyCoder <charawe.yw@gmail.com>
2023-10-07 10:45:38 +0300

    refactor and test

--- 0x03-python-data_structures/0-main.py
+++ 0x03-python-data_structures/0-main.py
@@ -3,3 +3,7 @@ print_list_integer = __import__("0-print_list_integer").print_list_integer
 
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
+my_list = []
+print_list_integer(my_list)
+my_list = [1, 2, "H", 9]
+print_list_integer(my_list)
