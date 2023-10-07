a969754afeecf820d33e14013a1b9a4e77ebe352
TheTinyCoder <charawe.yw@gmail.com>
2023-10-07 10:48:57 +0300

    refactor and test

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        if (type(i) is int):
-            print("{:d}".format(i))
+        # if (type(i) is int):
+        print("{:d}".format(i))
