9d206285abe5aa07e788627db1a94cc32549884e
TheTinyCoder <charawe.yw@gmail.com>
2023-10-09 14:00:02 +0300

    write a script that checks if a singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include "stdio.h"
+
+/**
+ * is_palindrome - function entry-point
+ *
+ * Description: checks if a singly linked list is a palindrome
+ * @head: pointer to singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	nodePtr temp;
+	int x = 0, y = 0, *z;
+	if (*head)
+	{
+		for (temp = *head; temp; temp = temp->next)
+			x++;
+		z = malloc(sizeof(int) * (x + 1));
+		if (!z)
+			return (0);
+		for (temp = *head; temp; temp = temp->next)
+			z[y] = temp->n, y++;
+		x -= 1;
+		for (y = 0; y != x && y > x; y++)
+		{
+			if (z[y] != z[x])
+			{
+				free(z);
+				return (0);
+			}
+			x--;
+		}
+		free(z);
+	}
+	return (1);
+}
