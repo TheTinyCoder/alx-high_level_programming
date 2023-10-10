5fe3cdeaba83e5f952e78250323ac97872d5623d
TheTinyCoder <charawe.yw@gmail.com>
2023-10-10 23:28:38 +0300

    resolve

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-# 7-add_item.py
-# Simon  Tagbor <simontagbor360@gmail.com>
-""" create a list from all arguments """
+"""
+Script that adds all arguments to a Python list,
+then saves them to a file
+"""
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+fname = "add_item.json"
+ListyBoii = []
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+try:
+    ListyBoii = load_from_json_file(fname)
+except:
+    pass
 
-    try:
-        arguments = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        arguments = []
-    arguments.extend(sys.argv[1:])
-    save_to_json_file(arguments, "add_item.json")
+for i in range(1, len(sys.argv)):
+    ListyBoii.append(sys.argv[i])
+save_to_json_file(ListyBoii, fname)
