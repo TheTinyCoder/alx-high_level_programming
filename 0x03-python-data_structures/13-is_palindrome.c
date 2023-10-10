383b356446977156cd9aacca971ced7916641002
TheTinyCoder <charawe.yw@gmail.com>
2023-10-10 12:29:21 +0300

    refactor to check for palindrome without malloc

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,29 +11,58 @@
 
 int is_palindrome(listint_t **head)
 {
-	nodePtr temp;
-	int x = 0, y = 0, *z;
+	nodePtr temp, temp1;
+	int x = 0, y = 0;
 
 	if (*head)
 	{
 		for (temp = *head; temp; temp = temp->next)
 			x++;
-		z = malloc(sizeof(int) * (x + 1));
-		if (!z)
-			return (0);
-		for (temp = *head; temp; temp = temp->next)
-			z[y] = temp->n, y++;
-		x -= 1;
-		for (y = 0; y < x; y++)
+		temp = *head;
+		if (x % 2 == 0)
 		{
-			if (z[y] != z[x])
-			{
-				free(z);
-				return (0);
-			}
-			x--;
+			for (y = 1; y < x / 2; y++)
+				temp = temp->next;
 		}
-		free(z);
+		else
+		{
+			for (y = 0; y < x / 2; y++)
+				temp = temp->next;
+		}
+		temp1 = reverse_list(&(temp->next));
+		temp = *head;
+		for (y = 0; y < x / 2; y++)
+		{
+			if (temp->n != temp1->n)
+				return (0);
+			temp = temp->next, temp1 = temp1->next;
+		}
 	}
 	return (1);
+}
+
+
+/**
+ * reverse_list - reverses a singly linked list
+ *
+ * @head: pointer to singly linked list
+ * Return: pointer to new head of list
+ */
+
+listint_t *reverse_list(listint_t **head)
+{
+	nodePtr temp1, temp2;
+
+	if (*head)
+	{
+		temp1 = *head;
+		while (temp1->next)
+		{
+			temp2 = *head;
+			*head = temp1->next;
+			temp1->next = (*head)->next;
+			(*head)->next = temp2;
+		}
+	}
+	return (*head);
 }
