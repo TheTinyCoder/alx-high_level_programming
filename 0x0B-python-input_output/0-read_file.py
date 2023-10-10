fe151eab1dcfd5b17186c35908164193505cfa11
TheTinyCoder <charawe.yw@gmail.com>
2023-10-10 23:15:07 +0300

    resolve

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """function that reads a text file and prints it"""
+"""
+Contains the read_file function
+"""
 
-    with open(filename) as f:
-        text = f.read()
-        print(text, end="")
+
+def read_file(filename=""):
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
