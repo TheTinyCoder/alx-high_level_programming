46d1025748007c7cfd6d415b33b768726abb3c21
TheTinyCoder <charawe.yw@gmail.com>
2023-10-13 17:28:41 +0300

    write function that converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -17,3 +17,9 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "XCIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,13 +4,27 @@
 
 
 def roman_to_int(roman_string):
-    roman_string = [i for i in roman_string.upper()]
+    roman_string = roman_string.upper()
+    set_1 = ('CM', 'CD', 'XC', 'XL', 'IX', 'IV')
+    roman_list = []
+    i = 0
+    while i < len(roman_string):
+        if (i + 1 < len(roman_string)):
+            str = roman_string[i] + roman_string[i + 1]
+            if str in set_1:
+                roman_list.append(str)
+                i += 2
+                continue
+        if (i < len(roman_string)):
+            roman_list.append(roman_string[i])
+        i += 1
+    print(roman_list)
     a_dict = {
         'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100,
         'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'VIII': 8,
         'VII': 7, 'VI': 6, 'V': 5, 'IV': 4, 'III': 3, 'II': 2, 'I': 1}
     number = 0
-    for i in roman_string:
+    for i in roman_list:
         for key, value in a_dict.items():
             if (i == key):
                 number = (value - number if (number < value)
