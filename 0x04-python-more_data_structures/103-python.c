d49692a6e6cdb75f6caa209eb8d4c574ee90dfe6
TheTinyCoder <charawe.yw@gmail.com>
2023-10-14 16:12:48 +0300

    create two C functions that print info about Python lists and Python bytes objects

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,66 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_python_bytes - function entry-point
+ *
+ * Description: prints first 10 bytes of object or error message if p is
+ * not a valid PyBytesObject
+ * @p: PyBytesObject
+ * Return: void
+ */
+
+void print_python_bytes(PyObject *p)
+{
+	int i, j, k;
+	const char *s, *t;
+
+	t = ((PyTypeObject *)(p)->ob_type)->tp_name;
+
+	if (strcmp(t, "bytes") == 0)
+	{
+		j = PyBytes_Size(p);
+		s = PyBytes_AsString(p);
+		k = j <= 10 ? j + 1 : 10;
+		printf("[.] bytes object info\n");
+		printf("  size: %d\n", j);
+		printf("  trying string: %s\n", s);
+		printf("  first %d bytes: ", k);
+		for (i = 0;  i < k - 1; i++)
+			printf("%.2hhx ", s[i]);
+		printf("%.2x\n", '\0');
+	}
+	else
+	{
+		printf("[.] bytes object info\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
+	}
+}
+
+/**
+ * print_python_list - function entry-point
+ *
+ * Description: prints some basic info about Python lists
+ * @p: list of subtype PyObject
+ * Return: void
+ */
+
+void print_python_list(PyObject *p)
+{
+	int i, j;
+	const char *s;
+	PyObject *q;
+
+	i = PyList_Size(p);
+	printf("[*] Size of the Python List = %d\n", i);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)(p))->allocated);
+	for (j = 0; j < i; j++)
+	{
+		q = PyList_GET_ITEM(p, j);
+		s = ((PyTypeObject *)(q)->ob_type)->tp_name;
+		printf("Element %d: %s\n", j, s);
+		if (strcmp(s, "bytes") == 0)
+			print_python_bytes(q);
+	}
+}
