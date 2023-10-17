6af5a5404dcea6ca17a478527af2d2236365def7
TheTinyCoder <charawe.yw@gmail.com>
2023-10-17 22:12:23 +0300

    create class with private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    Square class
+
+    Attributes:
+        size (int): length of the square
+    """
+    def __init__(self, size):
+        self.__size = size
