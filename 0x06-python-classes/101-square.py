81be8b6d5e6345d8ea5447474e1e3cb12730aa8e
TheTinyCoder <charawe.yw@gmail.com>
2023-10-18 14:59:22 +0300

    define __str__ for the Square class

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    Square class
+
+    Attributes:
+        __size (int): length of the square
+        __position (tuple of 2 positive int): position of the square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """
+        Getter method for size
+
+        Returns: size of Square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        """
+        Getter method for position
+
+        Returns: position of Square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if not isinstance(position, tuple) or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        for i in position:
+            if not isinstance(i, int) or i < 0:
+                raise TypeError(
+                    "position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        """
+        Computes area of Square
+
+        Returns: area
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """
+        Prints in stdout the square with the character #
+        """
+        size, position = self.__size, self.__position
+        if (size == 0):
+            print()
+        else:
+            for i in range(position[1]):
+                print()
+            for i in range(size):
+                print(" " * position[0], end="")
+                print("#" * size, end="")
+                print()
+
+    def __str__(self):
+        """
+        Defines how instance of Square will be printed on print()
+        """
+        square_string = ""
+        size, position = self.__size, self.__position
+        if (size == 0):
+            return (square_string)
+        else:
+            for i in range(position[1]):
+                square_string += "\n"
+            for i in range(size):
+                square_string += (" " * position[0]) + ("#" * size) + "\n"
+            return (square_string)
---
+++ 0x06-python-classes/tests/101-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+sys.path.append('../')
+Square = __import__('101-square').Square
+
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
