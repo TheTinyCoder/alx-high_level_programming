be3eef8e17e62c0d0dfab85dd9704ac419417afd
TheTinyCoder <charawe.yw@gmail.com>
2023-10-23 23:14:26 +0300

    write doctest for div function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,15 +26,14 @@ def matrix_divided(matrix, div):
         if not isinstance(row, list):
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
         for i in row:
             if not isinstance(i, int):
                 raise TypeError(
                     "matrix must be a matrix (list of lists) "
                     "of integers/floats")
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, int) and not isinstance(div, float):
+    if not type(div) in (int, float):
         raise TypeError("div must be a number")
     if (div == 0):
         raise ZeroDivisionError("division by zero")
