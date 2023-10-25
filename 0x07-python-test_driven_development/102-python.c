1c650d94512285678f9cd9b746346e99c13a49ee
TheTinyCoder <charawe.yw@gmail.com>
2023-10-25 19:27:47 +0300

    write C function that print info about Python string objects

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,32 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_python_string - function entry-point
+ *
+ * Description: prints string object or error message
+ * if p is not a valid string
+ * @p: PyObject
+ * Return: void
+ */
+
+void print_python_string(PyObject *p)
+{
+	int i, j, k;
+	const char *s, *t;
+
+	t = ((PyTypeObject *)(p)->ob_type)->tp_name;
+	printf("[.] string object info\n");
+
+	if (strcmp(t, "str") == 0)
+	{
+		j = PyUnicode_GetLength(p);
+		s = PyUnicode_AsUTF8(p);
+		printf("  type: compact unicode object\n");
+		printf("  length: %d\n", j);
+		printf("  value: %s\n", s);
+	}
+	else
+		printf("  [ERROR] Invalid String Object\n");
+}
