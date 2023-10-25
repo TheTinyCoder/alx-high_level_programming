f05fa7e05857ab9c1801b4e6f997f8afa510aed0
TheTinyCoder <charawe.yw@gmail.com>
2023-10-25 12:12:38 +0300

    update doctest for matrix multiplication function

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -27,17 +27,13 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    if len(m_a) is 0:
-        raise ValueError("m_a can't be empty")
-    if len(m_b) is 0:
-        raise ValueError("m_b can't be empty")
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-    if all(len(row) == 0 for row in m_a):
+    if len(m_a) is 0 or all(len(row) == 0 for row in m_a):
         raise ValueError("m_a can't be empty")
-    if all(len(row) == 0 for row in m_b):
+    if len(m_b) is 0 or all(len(row) == 0 for row in m_b):
         raise ValueError("m_b can't be empty")
     for row in m_a:
         if not all(type(i) in (int, float) for i in row):
