715007a3400e77cffb997b50ae84b6fd8313636d
TheTinyCoder <charawe.yw@gmail.com>
2023-10-30 12:49:29 +0300

    update perimeter method for rectangle class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -63,4 +63,6 @@ class Rectangle:
 
     def perimeter(self):
         """Returns perimeter of rectangle"""
+        if self.width == 0 or self.height == 0:
+            return (0)
         return (self.width + self.height) * 2
--- 0x08-python-more_classes/tests/2-main.py
+++ 0x08-python-more_classes/tests/2-main.py
@@ -11,3 +11,8 @@ print("--")
 my_rectangle.width = 10
 my_rectangle.height = 3
 print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print("--")
+
+my_rectangle = Rectangle(10)
+print("{} - {} => {} / {}".format(my_rectangle.width, my_rectangle.height, my_rectangle.area(), my_rectangle.perimeter()))
