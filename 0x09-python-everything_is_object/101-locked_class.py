d60a12c949440185113848e7fc46955f63a519c7
TheTinyCoder <charawe.yw@gmail.com>
2023-10-31 20:52:02 +0300

    create class that does not allow dynamic creation of attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,11 @@
 """
 Defines a class that doesn't allow dynamically created variables
 """
+
+
 class LockedClass():
+    """
+    Class with predefined attributes
+    """
     __slots__ = ["first_name"]
     pass
