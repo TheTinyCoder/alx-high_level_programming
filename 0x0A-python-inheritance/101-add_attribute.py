e89f5ef41fa2a26599b31a0fe4e081c6e5cfd814
TheTinyCoder <charawe.yw@gmail.com>
2023-11-02 21:38:35 +0300

    raise exception if new attribute cannot be added to an object

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""
+Defines add_attribute
+"""
+
+
 def add_attribute(a_class, attribute, value):
+    """
+    Adds a new attribute to an object if it’s possible
+    Args:
+        a_class (object)
+        attribute : name of attribute to add to object
+        value: value of the attribute being added
+    Raises:
+        TypeError if the object can’t have a new attribute
+    """
     if not hasattr(a_class, attribute):
         raise TypeError("can't add new attribute")
     a_class.attribute = value
