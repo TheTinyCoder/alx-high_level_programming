4b7dbc73b8ed43c9f81d9868f3e1d60c887b6e52
TheTinyCoder <charawe.yw@gmail.com>
2023-11-02 16:00:34 +0300

    create unimplemented function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    Empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
