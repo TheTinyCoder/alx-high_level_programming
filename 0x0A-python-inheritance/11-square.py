3b3cf88e14a6b22b49c3f7ce3e151b3553411cde
TheTinyCoder <charawe.yw@gmail.com>
2023-11-02 18:09:44 +0300

    override str method from parent

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines class Square that inherits from Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Inherits from Rectangle
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size):
+        if self.integer_validator("size", size) is None:
+            self.__size = size
+            super().__init__(size, size)
+
+    def __str__(self):
+        return (super().__str__().replace('Rectangle', "Square"))
