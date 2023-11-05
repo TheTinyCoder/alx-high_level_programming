d98b5bc35730d7cf8917c67e6c75940b0d416831
TheTinyCoder <charawe.yw@gmail.com>
2023-11-05 14:42:49 +0300

    write script that serializes objects in json and saves to file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module that converts/serializes object to json and saves to file
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Serializes objects in json and writes to text file
+    Args:
+        my_obj: object to serialize
+        filename: name of file to write to
+    Returns: JSON representation of an object
+    """
+    with open(filename, mode='w') as f:
+        json.dump(my_obj, f)
