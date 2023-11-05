c091744c340c2e8155307cd24d901c02f87b1bf0
TheTinyCoder <charawe.yw@gmail.com>
2023-11-05 19:32:55 +0300

    write script that serializes instances of a class in json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,14 +3,12 @@
 Module that converts/serializes instance of a Class to json
 """
 
-import json
-
 
 def class_to_json(obj):
     """
     Serializes instances of a Class in json
     Args:
         obj: object to serialize
-    Returns: JSON representation of an object
+    Returns: dictionary representation of obj
     """
-    return (json.dumps(obj.__dict__))
+    return obj.__dict__
