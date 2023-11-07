f14b32ccdc218c88c67f5a53c96d91c057314d98
TheTinyCoder <charawe.yw@gmail.com>
2023-11-07 21:44:28 +0300

    update rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 Rectangle Module
+Inherits from Base
 """
 from models.base import Base
 
@@ -17,7 +18,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes instances"""
-        super()__init__().id
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
