78df096231617c32203ecc937a814d9c1a32dba5
TheTinyCoder <charawe.yw@gmail.com>
2023-11-07 15:42:15 +0300

    parse logs from stdin and handle sigint

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,24 +5,23 @@ Module that reads stdin line by line and computes metrics
 
 import sys
 
-file_size = 0
-metrics = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
-           "405": 0, "500": 0}
-count = 0
+totalFileSize = 0
+status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 try:
     for line in sys.stdin:
         log = line.strip().split(' ')
-        file_size += int(log[-1])
-        if log[-2] in metrics.keys():
-            metrics[log[-2]] += 1
-        count += 1
-        if count % 10 == 0:
-            print(f"File size: {file_size}")
-            for (k, v) in metrics.items():
+        totalFileSize += int(log[-1])
+        if int(log[-2]) in status.keys():
+            status[int(log[-2])] += 1
+        line_count += 1
+        if line_count % 10 == 0:
+            print(f"File size: {totalFileSize}")
+            for (k, v) in sorted(status.items()):
                 if v != 0:
                     print(f"{k}: {v}")
 except KeyboardInterrupt:
-    print(f"File size: {file_size}")
-    for (k, v) in metrics.items():
+    print(f"File size: {totalFileSize}")
+    for (k, v) in sorted(status.items()):
         if v != 0:
             print(f"{k}: {v}")
