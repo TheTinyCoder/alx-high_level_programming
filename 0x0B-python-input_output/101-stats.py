bde19084b23725fd396dde5dc78918723036fadd
TheTinyCoder <charawe.yw@gmail.com>
2023-11-07 16:09:10 +0300

    parse logs from stdin and handle sigint

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,7 +10,7 @@ status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 try:
     for line in sys.stdin:
-        log = line.strip().split(' ')
+        log = line.strip().split()
         totalFileSize += int(log[-1])
         if int(log[-2]) in status.keys():
             status[int(log[-2])] += 1
