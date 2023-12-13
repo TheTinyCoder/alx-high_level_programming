1e64755696d6910f4d77e433a78bfd964d713b3a
TheTinyCoder <charawe.yw@gmail.com>
2023-12-13 10:21:21 +0300

    convert database and table to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- convert database, table and column to utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
