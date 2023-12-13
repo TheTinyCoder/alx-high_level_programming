e87b194f7ad9f6fbc9c00b78a41c25e5a612152e
TheTinyCoder <charawe.yw@gmail.com>
2023-12-13 10:43:03 +0300

    import table dump

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- import table dump from sql and display average
-SOURCE temperatures.sql
+-- SOURCE temperatures.sql
 SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp;
