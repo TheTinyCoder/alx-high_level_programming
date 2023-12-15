fffe81f2f1b03ece58335d2086e561329a7e63d5
TheTinyCoder <charawe.yw@gmail.com>
2023-12-15 09:06:15 +0300

    create table with autoincrement primary key id column

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- create table with autoincrement id which is the primary key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTOINCREMENT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
