c75e3fc4a2c2641a37365c9d5831005ccf4f4f55
TheTinyCoder <charawe.yw@gmail.com>
2023-12-15 10:24:48 +0300

    list all tv shows with a genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- list shows that have a genre id
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
