78e4f4882ffb50a762a2ee77ee093c6bd9a0f186
TheTinyCoder <charawe.yw@gmail.com>
2023-12-19 10:05:12 +0300

    lists all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,10 +9,10 @@ from MySQLdb import _mysql
 if __name__ == '__main__':
     db = _mysql.connect(
         user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
-cur = db.cursor()
-cur.execute("""SELECT * FROM states ORDER BY id""")
-states = cur.fetchmany()
-for state in states:
-    print(state)
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY id""")
+    states = cur.fetchmany()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
