bd41a18b0cfe90308d1e8ed6ec7de039293dedb5
TheTinyCoder <charawe.yw@gmail.com>
2023-12-20 10:48:02 +0300

    filter cities by their state name

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Filter cities by state name
+from the database hbtn_0e_4_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute(
+        """
+        SELECT cities.name, states.name FROM cities \
+        JOIN states ON states.id = cities.state_id \
+        WHERE states.name = %s
+        ORDER BY cities.id;
+        """, (sys.argv[4]))
+    cities = cur.fetchall()
+    for index, city in enumerate(cities):
+        print(city[0])
+        if index < cities.length - 1:
+            print(", ")
+    cur.close()
+    db.close()
