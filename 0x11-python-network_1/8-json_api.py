41684757f031ad03f9c66cfbb4bc44f3e107e44e
TheTinyCoder <charawe.yw@gmail.com>
2024-01-06 22:12:04 +0300

    resolve

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     try:
         data['q'] = sys.argv[1]
-    except:
+    except Exception:
         pass
 
     r = requests.post('http://0.0.0.0:5000/search_user', data)
