fb23c0461383866d57cd852b5e44649e730aada7
TheTinyCoder <charawe.yw@gmail.com>
2024-09-03 21:04:09 +0300

    script that lists all states in a database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+Script that lists all states from the database hbtn_0e_0_usa
 """
 
-import sys
 import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-        user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY id;""")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
-    cur.close()
-    db.close()
+args = sys.argv
+
+db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3])
+
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states");
+rows = cur.fetchall()
+for row in rows:
+    print((row))
+
+
