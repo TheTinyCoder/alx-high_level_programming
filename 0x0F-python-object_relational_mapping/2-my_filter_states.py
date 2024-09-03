c05ce31fa00bb229852fbc8a277763b7a1710f53
TheTinyCoder <charawe.yw@gmail.com>
2024-09-03 23:57:33 +0300

    adding name as parameter to query using str.format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
                          passwd=args[2], db=args[3])
     state = args[4]
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s", [state])
+    query = "SELECT * FROM states WHERE name = '{}'".format(state)
+    cur.execute(query)
     states = cur.fetchall()
     for row in states:
         print(row)
