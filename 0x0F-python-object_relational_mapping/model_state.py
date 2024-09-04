89ceb9bc2c5ab59cb8bd8e41803ea7eecbd5faa1
TheTinyCoder <charawe.yw@gmail.com>
2024-09-04 10:18:03 +0300

    pycodestyle compliance

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,14 +10,20 @@ import sys
 
 Base = declarative_base()
 
+
 class State(Base):
-    __tablename__='states'
-    id = Column(Integer, unique=True, autoincrement=True, nullable=False, primary_key=True)
+    """
+    Defines table states and its constraints
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, autoincrement=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
     args = sys.argv
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(args[1], args[2], args[3]))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(args[1], args[2], args[3]))
     engine.connect()
     Base.metadata.create_all(engine)
-
