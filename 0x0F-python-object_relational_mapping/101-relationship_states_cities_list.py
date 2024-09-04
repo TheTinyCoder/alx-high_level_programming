bbde17b9e7a632fa40b74becfd427c58326bdb37
TheTinyCoder <charawe.yw@gmail.com>
2024-09-05 00:18:44 +0300

    connect to db with sqlalchemy and list all States & corresponding Cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects and corresponding City objects
+from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from relationship_state import Base, State
+from relationship_city import City
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(args[1], args[2], args[3]))
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"\t{city.id}: {city.name}")
