d71d9743633b0fa4c28970bc0a249c9091e3c2be
TheTinyCoder <charawe.yw@gmail.com>
2024-09-04 13:36:58 +0300

    connect to db with sqlalchemy and list first row of states

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(args[1], args[2], args[3]))
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).first()
+    if state is not None:
+        print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
