ae4ff690311299ee333358771bb70352cffc9573
TheTinyCoder <charawe.yw@gmail.com>
2024-09-04 14:06:54 +0300

    connect to db with sqlalchemy and add the state “Louisiana” to the database

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that padds the State object “Louisiana”
+to the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(args[1], args[2], args[3]))
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+    print(state.id)
