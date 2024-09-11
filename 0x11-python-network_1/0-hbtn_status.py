5e259c4f6d56cdd651dc33f84435de7ea1c2352c
TheTinyCoder <charawe.yw@gmail.com>
2024-09-11 22:42:26 +0300

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status """
-import urllib.request
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
 
+
+from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        html = res.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
