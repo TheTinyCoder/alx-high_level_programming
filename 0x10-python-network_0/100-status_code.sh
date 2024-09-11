eb35dbfa224e63920dc9a31636cdd9661eb04d41
TheTinyCoder <charawe.yw@gmail.com>
2024-09-11 18:34:54 +0300

    display only the status code of the response without piping

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | grep -m1 "^HTTP" | cut -d" " -f2
+curl -s -o /dev/null -w "%{http_code}" "$1"
