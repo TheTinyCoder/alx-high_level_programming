a8c8ec9304b066f93032060fed49b4f5eefbfd73
TheTinyCoder <charawe.yw@gmail.com>
2024-09-12 19:08:04 +0300

    take repo & owner names and print 10 most recent commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that takes repo name and owner and prints
+10 most recent commits by: `<sha>: <author name>`
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(
+        f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits")
+    data = []
+    for commit in r.json():
+        data.append({
+            "name": commit['commit']['author']['name'],
+            "date": commit['commit']['author']['date'],
+            "sha": commit['sha']})
+    sorted_data = sorted(data, key=lambda x: x['date'])
+    for x in range(10):
+        print(f"{sorted_data[x]['sha']}: {sorted_data[x]['name']}")
