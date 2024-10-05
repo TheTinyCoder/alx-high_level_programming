277aed15f442cd525d68f5737a6cad094d790a54
TheTinyCoder <charawe.yw@gmail.com>
2024-10-05 11:31:50 +0300

    use function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-function add(a, b) {
+function add (a, b) {
   return (a + b);
 }
-if (args.length >= 2 && !args.some((x)=>isNaN(parseInt(x)))) {
+if (args.length >= 2 && !args.some((x) => isNaN(parseInt(x)))) {
   console.log(add(parseInt(args[0]), parseInt(args[1])));
 } else {
   console.log('NaN');
