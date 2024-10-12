169b5b417f74b271bf278141ebdae6fe91c3cd44
TheTinyCoder <charawe.yw@gmail.com>
2024-10-12 11:27:35 +0300

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('node:fs');
 const args = process.argv.slice(2);
-const data = fs.readFIleSync(args[0]).toString();
-const data1 = fs.readFIleSync(args[1]).toString();
+const data = fs.readFileSync(args[0]).toString();
+const data1 = fs.readFileSync(args[1]).toString();
 fs.writeFileSync(args[2], data + data1);
