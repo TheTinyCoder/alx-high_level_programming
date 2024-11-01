3e439b57cee7b7956d28a7b100b625c88f917a86
Nadu C <charawey@gmail.com>
2024-11-01 12:41:35 +0300

    display status code of get request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 const path = process.argv[2];
 request(path, (error, response) => {
-  if (!error) console.log("code: " + response.statusCode);
+  if (!error) console.log('code: ' + response.statusCode);
 });
