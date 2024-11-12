d4d3407ca98308d634817e4d48e078b8e5e9d2ba
Nadu C <charawey@gmail.com>
2024-11-12 11:05:46 +0300

    use jquery to append to, pop & empty list

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -2,4 +2,10 @@ $('document').ready(() => {
   $('#add_item').on('click', () => {
     $('.my_list').append('<li>Item</li>');
   });
+  $('#remove_item').on('click', () => {
+    $('.my_list li:last-child').remove();
+  });
+  $('#clear_list').on('click', () => {
+    $('.my_list').empty();
+  });
 });
